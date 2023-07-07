37f1d6f56234b8638d115183522d549bccb93cf6
lsacienne <alexandre.viala@outlook.com>
2023-07-07 18:22:59 +0200

    :sparkles: Define structures

---
+++ sudoku-v/local_modules/structures.ts
@@ -0,0 +1,117 @@
+export class Cell {
+    value: number | null;
+    readonly index: number;
+
+    constructor(value: null | number, index: number) {
+        this.value = value;
+        this.index = index;
+    }
+
+    computeCol(): number {
+        return this.index % 9;
+    }
+
+    computeLine(): number {
+        return Math.floor(this.index / 9);
+    }
+
+    computeSquare(): number {
+        return Math.floor(this.computeCol() / 3) + 3 * Math.floor(this.computeLine() / 3);
+    }
+
+    setValue(value: number) {
+        this.value = value;
+    }
+}
+
+class C {
+    index: number;
+    cells: Array<Cell>;
+
+    constructor(index: number) {
+        this.index = index;
+        this.cells = [];
+    }
+
+    addCell(cell: Cell) {
+        this.cells.push(cell);
+    }
+}
+
+class L {
+    index: number;
+    cells: Array<Cell>;
+
+    constructor(index: number) {
+        this.index = index;
+        this.cells = [];
+    }
+
+    addCell(cell: Cell) {
+        this.cells.push(cell);
+    }
+}
+
+class S {
+    index: number;
+    cells: Array<Cell>;
+
+    constructor(index: number) {
+        this.index = index;
+        this.cells = [];
+    }
+
+    addCell(cell: Cell) {
+        this.cells.push(cell);
+    }
+}
+
+export class Problem {
+    cols: Array<C>;
+    lins: Array<L>;
+    squs: Array<S>;
+    cells: Array<Cell>;
+
+    constructor() {
+        this.cols = [];
+        for (let i = 0; i < 9; ++i) {
+            this.cols.push(new C(i));
+        }
+        this.lins = [];
+        for (let i = 0; i < 9; ++i) {
+            this.lins.push(new L(i));
+        }
+        this.squs = [];
+        for (let i = 0; i < 9; ++i) {
+            this.squs.push(new S(i));
+        }
+        this.cells = [];
+    }
+
+    putCellInCol(cell: Cell) {
+        let colId = cell.computeCol();
+        this.cols.find(col => col.index === colId)?.addCell(cell);
+    }
+
+    putCellInLin(cell: Cell) {
+        let linId = cell.computeLine();
+        this.lins.find(lin => lin.index === linId)?.addCell(cell);
+    }
+
+    putCellInSqu(cell: Cell) {
+        let squId = cell.computeSquare();
+        this.squs.find(squ => squ.index === squId)?.addCell(cell);
+    }
+
+    addCell(cell: Cell) {
+        this.cells.push(cell);
+        this.putCellInCol(cell);
+        this.putCellInLin(cell);
+        this.putCellInSqu(cell);
+    }
+
+    printProblem() {
+        let lines = this.lins.map(lin => lin.cells.map(cell => cell.value));
+        console.log(lines);
+    }
+}
