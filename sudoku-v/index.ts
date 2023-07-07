8d1a69482d4a23d7c291c8fc8c836d6ab1aac4a9
lsacienne <alexandre.viala@outlook.com>
2023-07-08 00:46:45 +0200

    ✅ Add test for the algorithm

--- sudoku-v/index.ts
+++ sudoku-v/index.ts
@@ -1,9 +1,12 @@
 import * as fs from 'fs';
 import { analyzeGrid, fillGrid } from './local_modules/analyze_grid';
+import { generateAndTest } from './local_modules/ai';
 
 
 const input = fs.readFileSync('../test.txt', 'utf8');
 let gridAnalyzed = analyzeGrid(input);
 let problem = fillGrid(gridAnalyzed);
 problem.printProblem();
+let res = generateAndTest(problem);
+res?.printDenseSolution();
 
