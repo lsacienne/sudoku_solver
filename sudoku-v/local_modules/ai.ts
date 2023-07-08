a8aa73a5d6efefd2d028d34b53e38cd74875dc9a
lsacienne <alexandre.viala@outlook.com>
2023-07-08 18:10:50 +0200

    :sparkles: Add new heuristic in algorithm

--- sudoku-v/local_modules/ai.ts
+++ sudoku-v/local_modules/ai.ts
@@ -21,3 +21,26 @@ export function generateAndTest(problem: Problem): null | Problem {
         return problem;
     }
 }
+
+export function generateAndTest2(problem: Problem, cellIndex: number | undefined): null | Problem {
+    if (!problem.isComplete()) {
+        if (cellIndex === undefined) {
+            throw new Error("The problem is already complete !");
+        }
+        for (let i = 1; i <= 9; ++i) {
+            let newPb = problem.copy();
+            newPb.setValue(cellIndex, i);
+            if (newPb.isCorrect(cellIndex)) {
+                newPb.localUpdateScore(cellIndex);
+                let nextCell = newPb.findMostConstraints();
+                let res = generateAndTest2(newPb, nextCell);
+                if (res) {
+                    return res;
+                }
+            }
+        }
+        return null;
+    } else {
+        return problem;
+    }
+}
