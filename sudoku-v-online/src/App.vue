ce8f1fc5b73f94616a8e646aea5a354b9e7029ba
lsacienne <alexandre.viala@outlook.com>
2023-07-09 22:32:35 +0200

    :tada: Start vue project

---
+++ sudoku-v-online/src/App.vue
@@ -0,0 +1,63 @@
+<template>
+    <h1>Sudoku solver</h1>
+    <!-- <FirstAI></FirstAI> -->
+    <EfficientAI></EfficientAI>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import FirstAI from '@/components/FirstAI.vue';
+import EfficientAI from './components/EfficientAI.vue';
+
+export default defineComponent({
+    name: 'App',
+    components: {
+        //FirstAI,
+        EfficientAI
+    }
+});
+</script>
+
+<style>
+@font-face {
+    font-family: Roboto-Mono;
+    src: url('~@/assets/fonts/Roboto_Mono/RobotoMono-VariableFont_wght.ttf');
+    font-weight: normal;
+}
+
+@font-face {
+    font-family: Roboto-Mono-Italic;
+    src: url('~@/assets/fonts/Roboto_Mono/RobotoMono-Italic-VariableFont_wght.ttf');
+    font-weight: normal;
+}
+
+body {
+    background: url('~@/assets/background-desktop.png');
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+}
+
+#app {
+    font-family: Roboto-Mono, arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    background-color: aliceblue;
+    border: solid .6rem rgb(1, 20, 37);
+    border-radius: .8rem;
+    box-shadow: .3rem .3rem 0 rgb(1, 20, 37);
+    width: 95%;
+    align-self: center;
+    margin-top: 60px;
+}
+</style>
+
+<style scoped>
+h1 {
+    font-family: Roboto-Mono-Italic, arial, sans-serif;
+    font-weight: bold;
+}
+</style>
