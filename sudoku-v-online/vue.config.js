03008f08e4cbb74c8111de9583093f5058b983a9
lsacienne <alexandre.viala@outlook.com>
2023-07-10 14:34:59 +0200

    :fire: Remove statement

--- sudoku-v-online/vue.config.js
+++ sudoku-v-online/vue.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from '@vue/cli-service'
-export default defineConfig({
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === "production" ? "/sudoku_solver/" : "/",
 })
